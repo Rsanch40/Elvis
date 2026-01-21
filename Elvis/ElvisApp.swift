1009d687ec2adeb49abc7d30f2e81a03284390fc
Rafael <rsanch40@uic.edu>
2026-01-21 09:50:18 -0600

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Sanchez, Rafael on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
